0605a67f6c08dcdff67638646fc3d6cdd057338f
teemo19 <cmaldonado0@ucol.mx>
2018-10-12 19:15:55 -0400

    Libreria telnet agregada

--- rootkit.py
+++ rootkit.py
@@ -1,4 +1,5 @@
-import nmap
+import nmap,time,threading
+from telnetlib import Telnet
 
 class _nmap:
 	def __init__(self,segment,port):
@@ -35,3 +36,76 @@ class parsing:
 		D.sort()
 		SORTED=[str(A[i])+"."str(B[i])+"."str(C[i])+"."+str(D[i]) for i in range(len(split_ips))]
 		return SORTED
+
+class _telnet:
+	def __init__(self,ips,user,passwd):
+		self.ips=ips
+		self.user=user
+		self.passwd=passwd
+
+	def invoke_shell(self,tn):
+		read=str(tn.read_some())
+		while(1):
+			try:
+				read+=str(tn.read_some())
+			except:
+				break
+		if(read.find("Username: ")>=0 or read.nind("Password:")>=0):
+			tn.write(self.user+"\n")
+			time.sleep(0.1)
+			read_pwd=tn.read_some()
+			if(read_pwd.find("Password:")>=0):
+				tn.write(sef.passwd+"\n")
+				time.sleep(0.1)
+				read_login=tn.read_some()
+		try:
+			if(read_login.find("#")>=0):
+				return tn
+			else:
+				if(read_login.find(">")>=0):
+					tn.write("ena\n")
+					time.sleep(0.1)
+					tn.write(self.passwd+"\n")
+					time.sleep(0.1)
+					read_login=tn.read_some()
+					if(read_login.find("#")>=0):
+						return tn
+		except:
+			return None
+
+	def telnet_autorization(self,ip,output):
+		try:
+			telnet_client=Telnet(ip,23,10)
+			remote_conn=_telnet.invoke_shell(self,telnet_client)
+			output.append([ip,remote_conn])
+		except:
+			output.append([ip,None])
+
+	def telnet_conection(self):
+		Threads=[]
+		for i in range(len(self.ips)):
+			t=threading.Thread(target=_telnet.telnet_autorization,args=(self,self.ips[i],output,))
+			Threads.append(t)
+			Threads[i].start()
+		Threads=[Threads[i].join() for i in range(len(self.ips))]
+		return output
+
+	def write_telnet_work(self,remote_conn,data,output):
+		ip=telnet_autorization[0]
+		remote_conn=telnet_autorization[1]
+		for i in range(len(data)):
+			remote_conn.write(data[i]+"\n")
+			time.sleep(0.1)
+		result=""
+		while(result.find("exit")<0):
+			try:
+				result+=remote_conn.read_some()
+				time.sleep(0.1)
+			except:
+				pass
+		output.append([ip.result])
+
+	def write_telnet(self,telnet_autorization):
+		for i in range(len(telnet_autorization)):
+			t=threading.Thread(target=write_telnet_work,args=(remote_conn,data,output,))
+
